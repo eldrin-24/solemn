a2f832187a90a3fe755896421ff92159830c6ba4
eldrin soliman <erdsoliman24@gmail.com>
2026-02-13 21:35:39 +0800

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  base: "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/solemn/" : "/",
   plugins: [react()],
 }));
